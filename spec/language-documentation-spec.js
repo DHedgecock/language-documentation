9bc0bba413ae0909271f4fa54673eff9530da9e1
Dan Hedgecock <hedgecock.d@gmail.com>
2017-01-21 15:05:48 -0800

    :tada: Init package with grammar for documentation comment blocks :tada:

---
+++ spec/language-documentation-spec.js
@@ -0,0 +1,14 @@
+'use babel';
+
+import LanguageDocumentation from '../lib/language-documentation';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('LanguageDocumentationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
