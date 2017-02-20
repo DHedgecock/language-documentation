6dcf537c9f7d9adaa5d2e059d73e2ddf469a23ed
Dan Hedgecock <hedgecock.d@gmail.com>
2017-02-20 08:40:26 -0800

    Bugfix matching patterns :bug:

--- spec/fixtures/syntax_test.js
+++ spec/fixtures/syntax_test.js
@@ -5,20 +5,37 @@
  * Documentation Test
 // ^^^^^^^^^^^^^      comment.block.documentation
 //               ^^^^ comment.block.documentation
- * `'t'` `.` `/`
-// ^^^^^         markup.raw.inline.code.block
-//       ^^^     markup.raw.inline.code.block
-//           ^^^ markup.raw.inline.code.block
- * `mutli
- *
-// <- comment.block.documentation.punctuation
- * line` end
-// ^^^^^     markup.raw.inline.code.block
-//       ^^^ comment.block.documentation
+ * `'t'` `.` `/` `()`
+// ^^^^^              markup.raw.inline.code.block
+//       ^^^          markup.raw.inline.code.block
+//           ^^^      markup.raw.inline.code.block
+//               ^^^^ markup.raw.inline.code.block
+ * _'t'_ _._ _/_ _()_
+// ^^^^^             markup.italic
+//       ^^^         markup.italic
+//           ^^^     markup.italic
+//               ^^^ markup.italic
  * @method methodName
  // <- comment.block.documentation.punctuation
 // ^^^^^^^            entity.tag.doc storage.type.class.doc
 //         ^^^^^^^^^^ entity.tag.doc description.doc
  * @protected
- * @param {Object} classitem          Backing class item
+ * @param {type} name  Description
+// ^^^^^^                          entity.tag storage.type.class
+//        ^^^^^^                   entity.tag entity.type entity.name.type.instance
+//               ^^^^              entity.tag entity.type variable.name
+//                     ^^^^^^^^^^^ entity.tag description
+ * @param {type}  name  Description
+// ^^^^^^                           entity.tag storage.type.class
+//        ^^^^^^                    entity.tag entity.type entity.name.type.instance
+//                ^^^^              entity.tag entity.type variable.name
+//                      ^^^^^^^^^^^ entity.tag description
+ * @returns {Promise} Ajax call
+// ^^^^^^^^                     entity.tag storage.type.class
+//          ^^^^^^^^^           entity.tag entity.name.type.instance
+//                    ^^^^^^^^^ entity.tag description
+ * @return {Promise} Ajax call
+// ^^^^^^^                     entity.tag storage.type.class
+//         ^^^^^^^^^           entity.tag entity.name.type.instance
+//                   ^^^^^^^^^ entity.tag description
  */
