eb33a40679d6d288d015e1f795e7fd837d85fdf9
Dan Hedgecock <hedgecock.d@gmail.com>
2017-02-19 19:00:42 -0800

    Add grammar testing with atom-grammar-test

---
+++ spec/fixtures/syntax_test.js
@@ -0,0 +1,24 @@
+// SYNTAX TEST 'source.js.jsx'
+
+/**
+// <- comment.block.documentation punctuation.definition.comment
+ * Documentation Test
+// ^^^^^^^^^^^^^      comment.block.documentation
+//               ^^^^ comment.block.documentation
+ * `'t'` `.` `/`
+// ^^^^^         markup.raw.inline.code.block
+//       ^^^     markup.raw.inline.code.block
+//           ^^^ markup.raw.inline.code.block
+ * `mutli
+ *
+// <- comment.block.documentation.punctuation
+ * line` end
+// ^^^^^     markup.raw.inline.code.block
+//       ^^^ comment.block.documentation
+ * @method methodName
+ // <- comment.block.documentation.punctuation
+// ^^^^^^^            entity.tag.doc storage.type.class.doc
+//         ^^^^^^^^^^ entity.tag.doc description.doc
+ * @protected
+ * @param {Object} classitem          Backing class item
+ */
--- spec/language-documentation-spec.js
+++ spec/language-documentation-spec.js
@@ -1,14 +1,21 @@
 'use babel';
-
-import LanguageDocumentation from '../lib/language-documentation';
+import path from 'path';
+import grammarTest from 'atom-grammar-test';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('LanguageDocumentationView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
+describe('LanguageDocumentation', () => {
+  beforeEach(function() {
+    waitsForPromise(function() {
+      return atom.packages.activatePackage('language-documentation');
+    });
+    waitsForPromise(function() {
+      return atom.packages.activatePackage('language-babel');
+    });
   });
+
+  grammarTest(path.join(__dirname, 'fixtures/syntax_test.js'))
 });
